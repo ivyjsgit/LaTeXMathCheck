7aede63a5af0b130693907a6eddbeb50b9b4c0f9
Jonathan Ivy <ivyjs@hendrix.edu>
2018-01-30 19:51:03 -0600

    Added JavaDocs

--- src/main/java/EquationChecker.java
+++ src/main/java/EquationChecker.java
@@ -3,6 +3,11 @@ import com.udojava.evalex.Expression;
 import java.math.BigDecimal;
 
 public class EquationChecker {
+	/**
+	 * Check if provided equation matches computer answer.
+	 * @param providedEquation
+	 * @return
+	 */
     public static boolean checkEquation(String providedEquation){
         BigDecimal providedResult = EquationParser.afterEquals(providedEquation);
         String equation = EquationParser.beforeEquals(providedEquation);
--- src/main/java/EquationParser.java
+++ src/main/java/EquationParser.java
@@ -1,4 +1,3 @@
-import com.sun.deploy.util.ArrayUtil;
 import org.apache.commons.lang3.StringUtils;
 
 import java.math.BigDecimal;
@@ -6,12 +5,27 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class EquationParser {
+	/**
+	 * 
+	 * @param docAsString TeX document represented as a string
+	 * @return ArrayList of all found equations in the document.
+	 */
     public static ArrayList<String> getEquations(String docAsString){
      return new ArrayList(Arrays.asList(StringUtils.substringsBetween(docAsString,"$","$")));
     }
+    /**
+     * 
+     * @param equation
+     * @return the part of the equation before the equals sign
+     */
     public static String beforeEquals(String equation){
         return StringUtils.substringBefore(equation,"=");
     }
+    /**
+     * 
+     * @param equation
+     * @return part of the equation after the equal song
+     */
     public static BigDecimal afterEquals(String equation){
         return new BigDecimal(StringUtils.substringAfter(equation,"="));
     }
