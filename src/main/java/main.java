96e9c16d75d11946198613d654a30f1e2c239269
Jonathan Ivy <ivyjs@hendrix.edu>
2018-02-02 22:45:32 -0600

    Cleaned up merging branches
    After the merging of master and Testing, everything got messed up, so  I had to manually fix affected files.

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,17 +1,9 @@
 import java.util.ArrayList;
 
 public class main {
-<<<<<<< HEAD
+
     public static void main(String[] args) {
 
-=======
-    public static void main(String[] args){
-        String equation="$1+1=2$";
-        ArrayList<String> foundEquations = EquationParser.getEquations(equation);
-        for(String equationItem:foundEquations) {
-            System.out.println(EquationChecker.checkEquation(equationItem));
-        }
->>>>>>> master
     }
 
 }
